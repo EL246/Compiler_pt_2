470ee852068ed8409d661ec1ff5f9d1ad4a5f411
elana <ElanaL1@gmail.com>
2019-05-19 17:11:02 -0400

    refactoring

---
+++ src/com/company/tokens/Symbol.java
@@ -0,0 +1,12 @@
+package com.company.tokens;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class Symbol {
+
+    private static final List<String> SYMBOLS = Arrays.asList("{", "}", "(", ")", "[", "]", ".",
+            ",", ";", "+", "-", "*", "/", "&", "|", "<", ">", "=", "~");
+
+    private static final String CATEGORY_NAME = "symbol";
+}
