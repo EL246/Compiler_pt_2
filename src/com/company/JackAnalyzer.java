bb188b1df0a435323d7c5d7c5191092f8f8568fa
elana <ElanaL1@gmail.com>
2019-05-21 21:10:10 -0400

    update JackAnalyzer to handle directories and files

--- src/com/company/JackAnalyzer.java
+++ src/com/company/JackAnalyzer.java
@@ -10,21 +10,52 @@ import java.io.IOException;
 
 class JackAnalyzer {
     private JackCompilationEngine compilationEngine;
+    private String filePath;
 
-    JackAnalyzer(String filepath) throws TokenizerException, IOException {
-        File jackFile = new File(filepath);
+    JackAnalyzer(String filepath) {
+        this.filePath = filepath;
+    }
+
+    void run() throws IOException, TokenizerException {
+        getJackFiles(filePath);
+    }
+
+    private void getJackFiles(String filePath) throws IOException, TokenizerException {
+        File file = new File(filePath);
+        if (file.isDirectory()) {
+            File[] files = file.listFiles();
+            if (files != null) {
+                checkForFilesAndProcess(files);
+            }
+        } else if (file.isFile()) {
+            handleFile(file);
+        }
+
+    }
+
+    private void checkForFilesAndProcess(File[] files) throws IOException, TokenizerException {
+        for (File f : files) {
+            if (f.isFile() && f.getName().endsWith(".jack")) {
+                handleFile(f);
+                System.out.println("Found file: " + f.getName());
+            } else if (f.isDirectory()) {
+                getJackFiles(f.getPath());
+            }
+        }
+    }
+
+    private void handleFile(File jackFile) throws TokenizerException, IOException {
         JackTokenizer jackTokenizer = new JackTokenizer(jackFile);
 
         String newFileName = jackFile.getName().replace(".jack",".xml");
         System.out.println("output file: " + newFileName);
 
         this.compilationEngine = new JackCompilationEngine(jackTokenizer, new File(newFileName));
-
-        TokenizerTest tokenizerTest= new TokenizerTest(new JackTokenizer(jackFile));
-        tokenizerTest.createTokenXML(jackFile.getName().replace(".jack","T.xml"));
-    }
-
-    void run() throws IOException {
         compilationEngine.handle();
+
+//        TokenizerTest tokenizerTest= new TokenizerTest(new JackTokenizer(jackFile));
+//        tokenizerTest.createTokenXML(jackFile.getName().replace(".jack","T.xml"));
     }
+
+
 }
