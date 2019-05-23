5e64a62cff54d8532210af3c720de1733989c10f
elana <ElanaL1@gmail.com>
2019-05-22 20:58:19 -0400

    refactoring

--- src/com/company/JackAnalyzer.java
+++ src/com/company/JackAnalyzer.java
@@ -3,13 +3,11 @@ package com.company;
 import com.company.parser.JackCompilationEngine;
 import com.company.tokens.JackTokenizer;
 import com.company.tokens.TokenizerException;
-import com.company.tokens.TokenizerTest;
 
 import java.io.File;
 import java.io.IOException;
 
 class JackAnalyzer {
-    private JackCompilationEngine compilationEngine;
     private String filePath;
 
     JackAnalyzer(String filepath) {
@@ -47,15 +45,13 @@ class JackAnalyzer {
     private void handleFile(File jackFile) throws TokenizerException, IOException {
         JackTokenizer jackTokenizer = new JackTokenizer(jackFile);
 
-        String newFileName = jackFile.getName().replace(".jack",".xml");
+        String newFileName = jackFile.getName().replace(".jack", ".xml");
         System.out.println("output file: " + newFileName);
 
-        this.compilationEngine = new JackCompilationEngine(jackTokenizer, new File(newFileName));
+        JackCompilationEngine compilationEngine = new JackCompilationEngine(jackTokenizer, new File(newFileName));
         compilationEngine.handle();
 
 //        TokenizerTest tokenizerTest= new TokenizerTest(new JackTokenizer(jackFile));
 //        tokenizerTest.createTokenXML(jackFile.getName().replace(".jack","T.xml"));
     }
-
-
 }
